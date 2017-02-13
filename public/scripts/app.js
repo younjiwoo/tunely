c5c8c7a7ab8f7c52d03ee33be057d81130264c8a
nathanallen <thenathanator@gmail.com>
2017-02-13 11:34:16 -0800

    swap hbs for template string w/ embedded songs and CRUD buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -121,12 +121,74 @@ function renderMultipleAlbums(albums) {
   });
 }
 
+function renderSong(song){
+  return `<span>&ndash; (${song.trackNumber}) ${song.name} &ndash;</span>`
+}
+
 function renderAlbum(album) {
   console.log('rendering album', album);
-  var albumHtml = $('#album-template').html();
-  var albumsTemplate = Handlebars.compile(albumHtml);
-  var html = albumsTemplate(album);
-  $('#albums').prepend(html);
+
+  album.songsHtml = album.songs.map(renderSong).join("");
+
+
+  var albumHtml = (`
+    <div class="row album" data-album-id="${album._id}">
+
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-body">
+
+
+          <!-- begin album internal row -->
+            <div class='row'>
+              <div class="col-md-3 col-xs-12 thumbnail album-art">
+                <img src="images/800x800.png" alt="album image">
+              </div>
+
+              <div class="col-md-9 col-xs-12">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Album Name:</h4>
+                    <span class='album-name'>${album.name}</span>
+                  </li>
+
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Artist Name:</h4>
+                    <span class='artist-name'>${album.artistName}</span>
+                  </li>
+
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Released date:</h4>
+                    <span class='album-releaseDate'>${album.releaseDate}</span>
+                  </li>
+
+                  <li class="list-group-item">
+                    <h4 class="inline-header">Songs:</h4>
+                    ${album.songsHtml}
+                  </li>
+
+                </ul>
+              </div>
+
+            </div>
+            <!-- end of album internal row -->
+
+            <div class='panel-footer'>
+              <div class='panel-footer'>
+                <button class='btn btn-primary add-song'>Add Song</button>
+                <button class='btn btn-danger delete-album'>Delete Album</button>
+                <button class='btn btn-info edit-album'>Edit Album</button>
+                <button class='btn btn-success save-album hidden'>Save Changes</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end one album -->
+  `);
+  $('#albums').prepend(albumHtml);
 }
 
 // when the add song button is clicked, display the modal
