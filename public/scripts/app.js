312a80353d74c8bd7877d0bb17f3f4808e7fdc98
nathanallen <thenathanator@gmail.com>
2017-02-13 11:34:16 -0800

    swap hbs for template string

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,8 +25,50 @@ function renderMultipleAlbums(albums) {
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
   console.log('rendering album', album);
-  var albumHtml = $('#album-template').html();
-  var albumsTemplate = Handlebars.compile(albumHtml);
-  var html = albumsTemplate(album);
-  $('#albums').prepend(html);
+  var albumHtml = (`
+    <div class="row album">
+
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-body">
+
+
+          <!-- begin album internal row -->
+            <div class='row'>
+              <div class="col-md-3 col-xs-12 thumbnail album-art">
+                <img src="images/800x800.png" alt="album image">
+              </div>
+
+              <div class="col-md-9 col-xs-12">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Album Name:</h4>
+                    <span class='album-name'>${album.name}</span>
+                  </li>
+
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Artist Name:</h4>
+                    <span class='artist-name'>${album.artistName}</span>
+                  </li>
+
+                  <li class="list-group-item">
+                    <h4 class='inline-header'>Released date:</h4>
+                    <span class='album-releaseDate'>${album.releaseDate}</span>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+            <!-- end of album internal row -->
+
+            <div class='panel-footer'>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end one album -->
+  `);
+  $('#albums').prepend(albumHtml);
 }
