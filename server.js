4bcece89ea84ac3c620b2dd8f524dfe3a78a19a2
younjiwoo <younjiwoo@gmail.com>
2017-05-30 11:00:34 -0700

    set up server.js and connected to localhost

---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require('express'),
+    app = express();
+
+
+
+console.log('john and younji pair programming');
+
+app.listen(3000, function () {
+  console.log('we connected it to localhost 3000');
+});
